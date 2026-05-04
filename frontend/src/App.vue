3a0d886eec7fd589250fbda9a5a9c39822b8e532
Dario Wenger <dariowenger@macbook-pro-von-dario.home>
2026-05-04 17:03:23 +0200

    linter fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+
 import Component from './components/Component.vue';
 </script>
 
 <template>
+  <!-- eslint-disable-next-line vue/require-component-is -->
   <Component />
 </template>
